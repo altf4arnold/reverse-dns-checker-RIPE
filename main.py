acea0714dd7da779ea3083fea606dda6cfda6dce
altf4arnold <arnold.dech@adct.be>
2024-01-31 12:39:33 +0100

    Is that thing copied from stackoverflow? Maybe...

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ Script written by Arnold DECHAMPS for the RIPE LABS article :
 """
 import io, re
 import dns.resolver
+import dns.rrset
+import asyncio
+from dns.asyncresolver import Resolver
+from typing import Tuple
 
 
 def filemanager():
@@ -32,12 +36,13 @@ def sorter(rawlines):
                 objects.append({"domain": domain, "nameserver": dns, "dnssec": dnssec})
                 dnssec = []
                 dns = []
-            domain = line.replace("domain:         ","")
+            domain = line.replace("domain:         ", "")
         elif re.match(r'^\s*nserver:', line) is not None:
-            dns.append(line.replace("nserver:        ",""))
+            dns.append(line.replace("nserver:        ", ""))
         elif re.match(r'^\s*ds-rdata:', line) is not None:
-            dnssec.append(line.replace("ds-rdata:       ",""))
+            dnssec.append(line.replace("ds-rdata:       ", ""))
     return objects
+
 
 def statmaker(sorteddata):
     """
@@ -51,19 +56,54 @@ def statmaker(sorteddata):
             dnssecenabled += 1
     return dnssecenabled
 
+
+async def dns_query(domain: str, rtype: str = 'A', **kwargs) -> dns.rrset.RRset:
+    kwargs, res_cfg = dict(kwargs), {}
+    rs = Resolver(**res_cfg)
+    res: dns.resolver.Answer = await rs.resolve(domain, rdtype=rtype, **kwargs)
+    return res.rrset
+
+
+async def dns_bulk(*queries: Tuple[str, str], **kwargs):
+    ret_ex = kwargs.pop('return_exceptions', True)
+    coros = [dns_query(dom, rt, **kwargs) for dom, rt in list(queries)]
+    return await asyncio.gather(*coros, return_exceptions=ret_ex)
+
+
+async def dnstest(queries):
+    success = 0
+    res = await dns_bulk(*queries)
+    for i, a in enumerate(res):
+        if isinstance(a, Exception):
+            print(
+                f" [!!!] Error: Result {i} is an exception! Original query: {queries[i]} || Exception is: {type(a)} - {a!s} \n")
+            continue
+        else:
+            success += 1
+    return success
+
+
 def dnstester(sorteddata):
     """
     Takes the data and tests the DNS servers
     """
+    queries = []
     working = 0
+    runs = 0
+    status = 0
     for object in sorteddata:
-        try:
-            dns.resolver.resolve(object["domain"], "SOA")
-            working += 1
-        except Exception as e:
-            print(e)
-            print(object["domain"] + " Not Working !")
+        queries.append((object["domain"], "SOA"))
+        if runs == 10000:
+            runs = 0
+            working = working + asyncio.run(dnstest(queries))
+            print("object : " + str(status))
+            queries = []
+        else:
+            runs += 1
+        status += 1
     return working
+
+
 def main():
     """
     main function
@@ -76,6 +116,7 @@ def main():
     print("Total amount of objects      : " + str(len(data)))
     print("Total amount of working ones : " + str(operational))
     print("Total with DNSSEC enabled    : " + str(dnssec))
+
 
 if __name__ == '__main__':
     main()
